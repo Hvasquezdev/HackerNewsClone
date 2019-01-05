ca3d772b065f978972236a2efcd443b6ede41f41
Hector Vasquez <hvasquezdev@gmail.com>
2019-01-05 17:10:58 -0400

    Edit API Url to HTTPS

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 export const DEFAULT_QUERY = 'JavaScript';
 export const DEFAULT_HPP = '15';
-export const PATH_BASE = 'http://hn.algolia.com/api/v1';
+export const PATH_BASE = 'https://hn.algolia.com/api/v1';
 export const PATH_SEARCH = '/search';
 export const PARAM_SEARCH = '?query='
 export const PARAM_PAGE = '&page=';
